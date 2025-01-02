0b73abaafbf2f8d9e39d2f0e25efeb185b313ae4
Rutvik Ram <rutvikram23@gmail.com>
2025-01-02 17:17:38 +0000

    datatypes

---
+++ 01-basic/02-datatypes.js
@@ -0,0 +1,23 @@
+"use strict";  // treat all js code as newer version
+
+// alert(3 + 3)  // we are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log("ram")
+
+let age = 15 
+
+// dat types 
+
+// number = 6
+// bigint   
+// string = ""
+// boolean = true/false
+// null  - is a object
+// undefined
+// symbol = unique
+// object 
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null); // object
